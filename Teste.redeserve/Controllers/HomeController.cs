c90f30b258bdd44c284e07576eaa0b8a12fbc323
Matheus Avelino de Jesus <78377266+MatheusAV@users.noreply.github.com>
2021-06-09 18:08:37 -0300

    Adicionar arquivos de projeto.

---
+++ Teste.redeserve/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Teste.redeserve.Models;
+
+namespace Teste.redeserve.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHostingEnvironment _environment;
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public IActionResult questao4()
+        {
+            var clsTeste = new ClsTeste (){ Codigo = 1000, Descricao = "Teste"};
+            return Ok(clsTeste);
+        } 
+        
+        [HttpGet]
+        public async Task<IActionResult> BuscaCep(string valorCep)
+        {
+            try
+            {
+                var ws = new WSCorreios.AtendeClienteClient();
+                var resultado = await ws.consultaCEPAsync(valorCep);
+                return Ok(resultado);
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Imagem()
+        {
+            try
+            {
+                byte[] bytes;
+                string base64;
+                string pastaFotos = Path.Combine(_environment.WebRootPath, "Imgs");
+                string caminhoArquivo = Path.Combine(pastaFotos, "redeservice.jpg");
+                using (var fileStream = new FileStream(caminhoArquivo, FileMode.Open))
+                {
+                    
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        fileStream.CopyTo(memoryStream);
+                        bytes = memoryStream.ToArray();
+                        base64 = Convert.ToBase64String(bytes);
+                    }
+                }
+                return Ok("Imagem alterada para Base 64 com sucesso " + base64);
+            }
+            catch (System.Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Teste.redeserve/Models/ClsTeste.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teste.redeserve.Models
+{
+    public class ClsTeste
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+    }
+}
